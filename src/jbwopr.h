c7b4db1616fa7ba125adfba5a849dff9afe51cdd
Jonny Bergdahl <bergdahl@users.noreply.github.com>
2026-02-25 12:09:12 +0100

    Version 1.2.0

--- src/jbwopr.h
+++ src/jbwopr.h
@@ -22,7 +22,7 @@
 #include "effects/jbwopreffects.h"
 #include "jbwoprhelpers.h"
 
-#define LIBRARY_VERSION "1.1.3";
+#define LIBRARY_VERSION "1.2.0";
 
 /// @brief W.O.P.R. board version
 enum JBWoprBoardVariant {
